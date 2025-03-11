0e6b8f7ef0174bc0527a9fdc4bc17b94f797aeb2
AO <arvinjaysontamayocastro@gmail.com>
2025-03-12 02:16:28 +0800

    2.7 Reviewing the projects in VS Code Cleanup 2.8 Creating the Product Entity (Core)

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+// Derive from BaseEntityClass
+{
+    // public int Id { get; set; }
+    // accessor
+    // public: EF
+    // private: class
+    // protected: class, derived
+    // [Key]
+    // get set property, inside table, inside database
+
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+}
